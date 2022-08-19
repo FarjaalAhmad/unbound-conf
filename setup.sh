87f3212a29f1981525107551f14a7e026fc4b775
Farjaal Ahmad <farjaalahmad@gmail.com>
2022-08-19 05:04:43 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo rm -f /etc/unbound/unbound.conf
 sudo wget https://raw.githubusercontent.com/FarjaalAhmad/unbound-conf/main/unbound.conf -O /etc/unbound/unbound.conf
-wget https://raw.githubusercontent.com/FarjaalAhmad/unbound-conf/main/resolvers.txt -O ~/resolvers.txt
+echo "127.0.0.1:5335" > ~/resolvers.txt
 echo "net.core.rmem_max=1048576" >> /etc/sysctl.conf
 echo "nameserver 127.0.0.1" > /run/systemd/resolve/resolv.conf
 sudo ln -fs /run/systemd/resolve/resolv.conf /etc/resolv.conf
