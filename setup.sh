9a1cb8a81814c96d706cae698c6c91a07d236d15
Farjaal Ahmad <farjaalahmad@gmail.com>
2022-08-19 05:09:54 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ sudo wget https://raw.githubusercontent.com/FarjaalAhmad/unbound-conf/main/unbou
 echo "127.0.0.1:5335" > ~/resolvers.txt
 echo "net.core.rmem_max=1048576" >> /etc/sysctl.conf
 echo "nameserver 127.0.0.1" > /run/systemd/resolve/resolv.conf
+echo "nameserver 1.1.1.1" >> /run/systemd/resolve/resolv.conf
 sudo ln -fs /run/systemd/resolve/resolv.conf /etc/resolv.conf
 echo 'alias dig="dig @localhost -p 5335"' >> ~/.bashrc
 source ~/.bashrc
